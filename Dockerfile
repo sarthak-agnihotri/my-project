a45d3c32af22242dbea0497b136d259315b2a769
sarthak-agnihotri <sarthak0144@gmail.com>
2026-05-23 11:50:07 +0530

    Added Python Fask CI-CD

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,14 @@
 # COPY . .
 # CMD ["python", "app.py"]
 
-FROM amazoncorretto:17
+# FROM amazoncorretto:17
+# WORKDIR /app
+# COPY App.java .
+# RUN javac App.java
+# CMD ["java", "App"]
+
+FROM python:3.11
 WORKDIR /app
-COPY App.java .
-RUN javac App.java
-CMD ["java", "App"]
+COPY . . # Copy the contents of the current directory (where the Dockerfile is located) into the /app directory in the container. This includes both the app.py and requirements.txt files, as well as any other files in the current directory.
+RUN pip install -r requirements.txt
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Python Docker App Running Successfully!")
+# print("Python Docker App Running Successfully!")
+
+from flask import Flask #import os
+app=Flask(__name__) # create an instance of the Flask class, which will be our WSGI application. The first argument is the name of the application's module or package. If you are using a single module (as in this case), you should use __name__ because depending on if it's started as application or imported as module the name will be different ('__main__' versus the actual import name). This is needed so that Flask knows where to look for resources such as templates and static files.
+@app.route("/")
+def home():
+    return "Python Docker App Running Successfully!"
+if __name__=="__main__": # check if the script is being run directly (as the main program) and not imported as a module in another script. If this condition is true, the code block under this statement will be executed. This is a common Python idiom for making code only run when the script is executed directly, and not when it is imported.
+    app.run(host="0.0.0.0", port=5000) # starts the Flask development server. The host parameter is set to "0.0.0.0" to make the server accessible from outside the container. The port parameter is set to 5000, which is the default port for Flask applications. This means that the application will be accessible at http://localhost:5000 when running on your local machine, or at the appropriate IP address and port when running in a Docker container.
+    
